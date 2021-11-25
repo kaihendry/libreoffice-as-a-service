48e88359e672817f0d8e694bd7fb202b252823b5
AJ ONeal <coolaj86@gmail.com>
2021-11-25 01:06:39 -0700

    f: typo

--- scripts/builder/deploy.sh
+++ scripts/builder/deploy.sh
@@ -5,7 +5,7 @@ set -u
 export GIT_REF_NAME="dev"
 export GIT_REPO_ID="savvi-legal/libreoffice-as-a-service"
 export GIT_REPO_NAME="libreoffice-as-a-service"
-export DNS_SUBDOMIN=dev-lass
+export DNS_SUBDOMIN=dev-laas
 export DNS_ZONE=savvy.legal
 
 bash .gitdeploy/deploy.sh
